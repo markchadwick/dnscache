605530aa4de2adfe72087203723776734bd1f3b0
Mark Chadwick <mark@vistarmedia.com>
2013-09-24 23:27:09 -0400

    Round-tripper reminder

--- dnscache_test.go
+++ dnscache_test.go
@@ -10,6 +10,12 @@ func init() {
 	log.SetFlags(log.Lshortfile | log.Ldate | log.Ltime)
 }
 
+var _ = spec.Suite("DNS Cache", func(c *spec.C) {
+	c.It("should provide a net/http RoundTripper", func(c *spec.C) {
+		c.Skip("not implemented")
+	})
+})
+
 func Test(t *testing.T) {
 	spec.Run(t)
 }
